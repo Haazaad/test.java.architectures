fb833035753bce393e82a14c0dd7dc439cdf4e56
Artem <smirnov.artem.sam@gmail.com>
2021-12-08 12:12:19 +0400

    Initial commit

---
+++ src/test/java/ru/haazad/java/architectures/java/architectures/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.haazad.java.architectures.java.architectures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

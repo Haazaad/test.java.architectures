aa25ae8ebc945466f8b382f1bac39e88f6efcfe1
Artem <smirnov.artem.sam@gmail.com>
2021-12-27 11:16:05 +0400

    + Added rabbit config

--- core-service/src/main/java/ru/haazad/java/architectures/adapters/RabbitmqAdapter.java
+++ core-service/src/main/java/ru/haazad/java/architectures/adapters/RabbitmqAdapter.java
@@ -4,14 +4,9 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -23,21 +18,6 @@ public class RabbitmqAdapter {
 
     @Autowired
     private RabbitTemplate rabbitTemplate;
-
-    @Bean
-    public Queue queue() {
-        return new Queue(QUEUE, false);
-    }
-
-    @Bean
-    public TopicExchange exchange() {
-        return new TopicExchange("webapp_exchange");
-    }
-
-    @Bean
-    public Binding binding(Queue queue, TopicExchange exchange) {
-        return BindingBuilder.bind(queue).to(exchange).with(QUEUE);
-    }
 
     @RabbitListener(queues = QUEUE)
     public void consume(String in) {
